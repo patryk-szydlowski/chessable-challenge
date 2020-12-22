8a1455d158f095a8c8c1d969e4082efe930f3bfe
patrykszydlowski <patrykszydlowski99@gmail.com>
2020-12-22 14:46:15 +0100

    ui tests

--- src/features/chessboard/components/board/chessboard-tile-container.test.tsx
+++ src/features/chessboard/components/board/chessboard-tile-container.test.tsx
@@ -6,9 +6,15 @@ import {chessboardSlice} from "../../utils"
 import {render} from "@testing-library/react"
 import {Provider as StoreProvider} from "react-redux"
 import {ChessboardTileContainer} from "./chessboard-tile-container"
+import {interactWithBoard} from "../../store";
+import userEvent from "@testing-library/user-event";
 
 jest.mock("features/piece/components", () => ({
   Piece: () => <div data-testid="piece"/>
+}))
+
+jest.mock("./chessboard-tile-highlight", () => ({
+  ChessboardTileHighlight: () => <div data-testid="highlight"/>
 }))
 
 describe("chessboard tile container", () => {
@@ -52,5 +58,86 @@ describe("chessboard tile container", () => {
 
     // expect
     expect(queryByTestId("piece")).not.toBeInTheDocument()
+  })
+
+  test("renders chessboard tile with highlight when tile position is a legal move for selected piece", () => {
+    // given
+    const piece: Piece = {
+      id: 1,
+      type: PieceType.PAWN,
+      color: PieceColor.WHITE,
+      position: {x: 0, y: 0},
+      specialStates: Set()
+    }
+
+    const store = mockStore(
+      chessboardSlice({
+        pieces: Map([[piece.id, piece]]),
+        selectedPieceId: piece.id
+      })
+    )
+
+    const {getByTestId} = render(
+      <StoreProvider store={store}>
+        <ChessboardTileContainer row={1} column={0}/>
+      </StoreProvider>
+    )
+
+    // expect
+    expect(getByTestId("highlight")).toBeInTheDocument()
+  })
+
+  test("renders chessboard tile without highlight when tile position is not a legal move for selected piece", () => {
+    // given
+    const piece: Piece = {
+      id: 1,
+      type: PieceType.PAWN,
+      color: PieceColor.WHITE,
+      position: {x: 0, y: 0},
+      specialStates: Set()
+    }
+
+    const store = mockStore(
+      chessboardSlice({
+        pieces: Map([[piece.id, piece]]),
+        selectedPieceId: piece.id
+      })
+    )
+
+    const {queryByTestId} = render(
+      <StoreProvider store={store}>
+        <ChessboardTileContainer row={5} column={5}/>
+      </StoreProvider>
+    )
+
+    // expect
+    expect(queryByTestId("highlight")).not.toBeInTheDocument()
+  })
+
+  test("dispatches action to interact with board when chessboard tile is clicked", () => {
+    // given
+    const piece: Piece = {
+      id: 1,
+      type: PieceType.PAWN,
+      color: PieceColor.WHITE,
+      position: {x: 0, y: 0},
+      specialStates: Set()
+    }
+
+    const store = mockStore(chessboardSlice({
+      pieces: Map([[piece.id, piece]]),
+    }))
+
+    const {getByTestId} = render(
+      <StoreProvider store={store}>
+        <ChessboardTileContainer row={0} column={0}/>
+      </StoreProvider>
+    )
+
+    // when
+    userEvent.click(getByTestId("piece"))
+
+    // then
+    expect(store.getActions()).toEqual([interactWithBoard({interactionPosition: piece.position})])
   })
 })
